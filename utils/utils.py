f070979f080f729f8fd2bbce19643f2ee40c0ecd
velajua <51091959+velajua@users.noreply.github.com>
2023-01-22 18:58:55 -0500

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -8,9 +8,10 @@ import threading
 import _thread as thread
 
 from math import factorial
-from typing import Callable
 from functools import wraps
 from datetime import datetime
+
+from typing import Callable, List, Tuple, Union, Dict
 
 
 def retry_decorator(max_retries: int) -> Callable:
@@ -45,9 +46,21 @@ def retry_decorator(max_retries: int) -> Callable:
     return retry_decorator_
 
 
+def timed_retries(max_retries: int,
+                  minutes: int = 1) -> Callable:
+    """
+    A decorator that allows a function to retry a
+    specified number of times after a specified time
+    interval in case of an exception
 
-def timed_retries(max_retries, minutes=1):
-    def retry_decorator(f):
+    :param max_retries: The maximum number of times to retry
+    :type max_retries: int
+    :param minutes: The time interval between retries in minutes
+    :type minutes: int
+    :return: The decorated function
+    :rtype: Callable
+    """
+    def retry_decorator(f: Callable) -> Callable:
         @wraps(f)
         def func_with_retries(*args, **kwargs):
             error = None
@@ -66,17 +79,35 @@ def timed_retries(max_retries, minutes=1):
     return retry_decorator
 
 
-def exit_after(s):
-    def quit_function(f_name):
+def exit_after(seconds: int) -> Callable:
+    """
+    A decorator that exits the program
+    if the function takes more than seconds seconds
+
+    :param seconds: The time limit in seconds
+    :type seconds: int
+    :return: The decorated function
+    :rtype: Callable
+    """
+    def quit_function(f_name: str):
+        """
+        A function that aborts the main thread
+        and prints an error message
+
+        :param f_name: The name of the function
+            that is being decorated
+        :type f_name: str
+        """
         print(f'{f_name} aborted', file=sys.stderr)
         sys.stderr.flush()
         thread.interrupt_main()
 
-    def outer(f):
+    def outer(f: Callable) -> Callable:
         @wraps(f)
         def inner(*args, **kwargs):
-            timer = threading.Timer(s, quit_function,
-                                    args=[f.__name__])
+            timer = threading.Timer(
+                seconds, quit_function,
+                args=[f.__name__])
             timer.start()
             try:
                 result = f(*args, **kwargs)
@@ -87,7 +118,15 @@ def exit_after(s):
     return outer
 
 
-def execution_time(f):
+def execution_time(f: Callable) -> Callable:
+    """
+    A decorator that calculates the execution time of a function
+
+    :param f: The function to be decorated
+    :type f: Callable
+    :return: The decorated function
+    :rtype: Callable
+    """
     @wraps(f)
     def get_execution_time(*args, **kwargs):
         initial = datetime.now()
@@ -98,38 +137,91 @@ def execution_time(f):
     return get_execution_time
 
 
-def remove_duplicates(f):
+def remove_duplicates(f: Callable) -> Callable:
+    """
+    A decorator that removes duplicates from
+    a list passed to the decorated function
+
+    :param f: The function to be decorated
+    :type f: Callable
+    :return: The decorated function
+    :rtype: Callable
+    """
     @wraps(f)
     def func_no_duplicates(*args):
         if type(*args) == list:
-            return f(list(dict.fromkeys(*args)))
+            args = list(dict.fromkeys(*args))
         return f(*args)
     return func_no_duplicates
 
 
-def levenshtein(word1, word2):
-    def min_dist(s1, s2):
-        if s1 == len(word1) or s2 == len(word2):
-            return len(word1) - s1 + len(word2) - s2
+def levenshtein(word1: str, word2: str) -> int:
+    """
+    A function that calculates the Levenshtein
+    distance (or edit distance) between two strings.
+    The Levenshtein distance is the minimum number of
+    single-character edits (insertions, deletions or substitutions)
+    required to change one string into the other.
+
+    :param word1: The first string
+    :type word1: str
+    :param word2: The second string
+    :type word2: str
+    :return: The Levenshtein distance between word1 and word2
+    :rtype: int
+    """
+    def min_dist(s1: int, s2: int) -> int:
+        """
+        A helper function that calculates the minimum
+        distance between the two strings
+        :param s1: The current position of the pointer in word1
+        :type s1: int
+        :param s2: The current position of the pointer in word2
+        :type s2: int
+        :return: The minimum distance between the two strings
+        :rtype: int
+        """
+        if s1 == len(word1):
+            return len(word2) - s2
+        if s2 == len(word2):
+            return len(word1) - s1
         if word1[s1] == word2[s2]:
-            return min_dist(s1+1, s2+1)
-        output = 1 + min(min_dist(s1, s2+1),
-                         min_dist(s1+1, s2),
-                         min_dist(s1+1, s2+1))
+            return min_dist(s1 + 1, s2 + 1)
+        output = 1 + min(min_dist(s1, s2 + 1),
+                         min_dist(s1 + 1, s2),
+                         min_dist(s1 + 1, s2 + 1))
         return output
 
     return min_dist(0, 0)
 
 
-def category_mapper(master_list, list_to_map, type_='both'):
-    assert type_ in ['both', 'ans',
-                     'filled'], 'Not a value for type_'
+def category_mapper(master_list: List[str],
+                    list_to_map: List[str],
+                    type_: str = 'both') -> Union[
+                        Tuple[List[str], List[str]], List[str]]:
+    """
+    A function that maps the elements of a list to
+    the closest matching elements in a master list
+    :param master_list: The master list to map the
+        elements of list_to_map to
+    :type master_list: List[str]
+    :param list_to_map: The list to map the elements
+        of to the master list
+    :type list_to_map: List[str]
+    :param type_: The type of output to return,
+        can be 'both' (default), 'ans', or 'filled'
+    :type type_: str
+    :return: A tuple of two lists (ans, filled) if type_ is 'both',
+        or a list of matched elements if type_ is 'ans' or 'filled'
+    :rtype: Union[Tuple[List[str], List[str]], List[str]]
+    """
+    assert type_ in ['both', 'ans', 'filled'
+                     ], f'Invalid value for type_: {type_}'
     ans, filled = [], []
     for i in list_to_map:
-        ans.append(value_[0] if (
-            value_ := difflib.get_close_matches(
-                i, master_list)) else '')
-        filled.append(value_[0] if value_ else i)
+        match = difflib.get_close_matches(i, master_list)
+        ans.append(match[0] if match else '')
+        filled.append(match[0] if match else i)
     if type_ == 'both':
         return ans, filled
     elif type_ == 'ans':
@@ -138,8 +230,29 @@ def category_mapper(master_list, list_to_map, type_='both'):
         return filled
 
 
-def find_rank(st):
-    def find_smaller_right(st, low, high):
+def find_rank(st: str) -> int:
+    """
+    A function that finds the lexicographic rank of a string
+    :param st: The string to find the rank of
+    :type st: str
+    :return: The lexicographic rank of the string
+    :rtype: int
+    """
+    def find_smaller_right(st: str, low: int,
+                           high: int) -> int:
+        """
+        A helper function that finds the number of characters
+        that are smaller than the character at the left index
+        :param st: The input string
+        :type st: str
+        :param low: The left index
+        :type low: int
+        :param high: The right index
+        :type high: int
+        :return: The number of characters that are
+            smaller than the character at the left index
+        :rtype: int
+        """
         count_r = 0
         i = low + 1
         while i <= high:
@@ -159,46 +272,118 @@ def find_rank(st):
     return rank
 
 
-def remove_symbols(x):
+def remove_symbols(x: str) -> str:
+    """
+    A function that removes symbols from a string
+    :param x: The input string
+    :type x: str
+    :return: The input string with symbols removed
+    :rtype: str
+    """
+    # Removes accents and other diacritical marks
+    # from the string
     x = unidecode.unidecode(x)
     return re.sub(r'[^\w ]', '', x)
 
 
-def check_value(x, default=None):
+def check_value(x: any, default: any = None
+                ) -> Tuple[any, bool]:
+    """
+    A function that checks if a value is not None,
+    returns a tuple containing the value and a boolean
+    indicating if the value is not None
+    :param x: The value to check
+    :type x: any
+    :param default: The default value to return
+        if x is None
+    :type default: any
+    :return: A tuple containing the value and a boolean
+        indicating if the value is not None
+    :rtype: Tuple[any, bool]
+    """
     return (x, True) if x else (default, False)
 
 
-def int_safe_cast(x):
+def int_safe_cast(x: any) -> Union[int, None]:
+    """
+    A function that safely converts a value to int
+    and returns None if the conversion is not possible
+    :param x: The value to convert
+    :type x: any
+    :return: The converted int value or None
+    :rtype: Union[int, None]
+    """
     if x is not None:
+        # Removes all non-numeric characters
+        # from the input value
         x = re.sub('[^0-9.-]', '', str(x))
+        # Removes the decimal part of the input value
         x = x.partition('.')[0]
         if x.isnumeric():
             return int(x)
     return None
 
 
-def float_safe_cast(x):
+def float_safe_cast(x: any) -> Union[float, None]:
+    """
+    A function that safely converts a value to float
+    and returns None if the conversion is not possible
+    :param x: The value to convert
+    :type x: any
+    :return: The converted float value or None
+    :rtype: Union[float, None]
+    """
     if x is not None:
+        # Removes all non-numeric characters
+        # from the input value
         x = re.sub('[^0-9.-]', '', str(x))
         try:
             return float(x)
-        except:
+        except Exception:
             return None
     return None
 
 
-def remove_spaces(x):
-    return re.sub('\s', '', str(x))
+def remove_spaces(x: any) -> str:
+    """
+    A function that removes all whitespaces from a string
+    :param x: The value to remove spaces from
+    :type x: any
+    :return: The input value with all spaces removed
+    :rtype: str
+    """
+    return re.sub(r'\s', '', str(x))
 
 
-def word_replacer(sentence, data):
+def word_replacer(sentence: str,
+                  data: Dict[str, str]) -> str:
+    """
+    A function that replaces words in a sentence with the
+    corresponding values from the provided data dictionary.
+    :param sentence: The input sentence
+    :type sentence: str
+    :param data: A dictionary containing the words to be
+        replaced as keys and their replacement values as values
+    :type data: Dict[str, str]
+    :return: The modified sentence
+    :rtype: str
+    """
     for k, v in data.items():
-        sentence = re.sub(f'(?:\s+|^){k}(?:\s+|$)',
+        sentence = re.sub(fr'(?:\s+|^){k}(?:\s+|$)',
                           f' {v} ', sentence)
-    return sentence
+    return sentence.strip()
 
 
-def separate_numbers_letters(x):
+def separate_numbers_letters(x: str) -> str:
+    """
+    A function that separates numbers and letters
+    in a string by adding a space between them
+    :param x: The input string
+    :type x: str
+    :return: The modified string with numbers and
+        letters separated by a space
+    :rtype: str
+    """
     return ' '.join(re.findall(
         '[0-9]+|[a-zA-Z]+', x))
 
