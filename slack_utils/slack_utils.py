ff7991df923b265e47a20b652ca4c0b6a292cac5
Juan Velasquez <51091959+velajua@users.noreply.github.com>
2023-03-10 11:15:16 -0500

    Update slack_utils.py

--- slack_utils/slack_utils.py
+++ slack_utils/slack_utils.py
@@ -49,7 +49,53 @@ def slackMessage(message: Union[str, Dict], url: str,
     response = requests.post(url, data=json.dumps(slack_data),
                              headers=headers)
     if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
+        return ('Error', response.status_code, response.text)
+
+
+def send_message(channel: str, text: str,
+                 TOKEN: str = None) -> str:
+    """
+    Sends a message to a Slack channel using the Slack Web API.
+    follow: https://www.pragnakalp.com/
+create-slack-bot-using-python-tutorial-with-examples/
+    for steps.
+
+    Parameters:
+    - channel (str): the name or ID of the
+        channel to send the message to.
+    - text (str): the message text to send.
+    - TOKEN (str): the Bot User OAuth Access
+        Token for your Slack app (required).
+
+    Returns:
+    - A string representation of the JSON
+        response from the Slack API.
+
+    Example usage:
+    ```
+    TOKEN = "your-token"
+    send_message(channel="general",
+                 text="Hello from Python!", TOKEN=TOKEN)
+    ```
+    """
+    if not TOKEN:
+        return "Error: No Slack token provided."
+    url = "https://slack.com/api/chat.postMessage"
+    headers = {
+        "Content-type": "application/json",
+        "Authorization": "Bearer {}".format(TOKEN),
+    }
+    json = {"channel": channel, "text": text}
+    typing_url = "https://slack.com/api/chat.postMessage"
+    typing_json = {
+        "channel": channel,
+        "text": "",
+        "blocks": [{"type": "section",
+                    "text": {"type": "mrkdwn", "text": "Typing..."}}],
+    }
+    requests.post(url=typing_url, headers=headers, json=typing_json)
+    response = requests.post(url=url, headers=headers, json=json)
+    return response.text
 
 
 file_name = os.path.splitext(os.path.basename(os.path.abspath(__file__)))[0]
